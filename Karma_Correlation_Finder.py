cba863934550395f008a0777d0cc09f6c0133fb0
Henry Tischler <mysterykeven@gmail.com>
2022-01-01 10:18:52 -0400

    finished the Karma-Correlation Finder

--- Karma_Correlation_Finder.py
+++ Karma_Correlation_Finder.py
@@ -1,11 +1,21 @@
+from pandas.core.reshape.concat import concat
+import matplotlib as plt
 from PushshiftIO import PushshiftIO
 import pandas as pd
 import numpy as np
-trials = 10000
+trials = 0
 accounts = []
 
-with open("69M_reddit_accounts.csv", "r") as user_list:
-    for i in range(trials):
-        accounts.append(PushshiftIO.read_specific_line(randint(1, 69382539), user_list))
-value = PushshiftIO.get_random_users(10000)
-print(value)
+finalData = pd.DataFrame(columns=["user", "karma", "Total Words", "Total Characters"])
+
+subjects = PushshiftIO.get_random_users(trials)
+dataMatrix = []
+
+for x in subjects:
+    content = PushshiftIO.get_user_comments(x) + " " + PushshiftIO.get_user_submissions(x)
+    user_statistics = [x, len(content), len(content.split())]
+    dataMatrix.append(user_statistics)
+
+finalData.concat(dataMatrix)
+finalData.to_csv("final_results.csv")
+
