ac586da21fa0062dbdad9af4b770f4bdf1688735
Henry Tischler <mysterykeven@gmail.com>
2022-01-02 13:46:04 -0400

    Fixed username bug in saved csv

--- Karma_Correlation_Finder.py
+++ Karma_Correlation_Finder.py
@@ -4,7 +4,7 @@ from PushshiftIO import PushshiftIO
 import pandas as pd
 import numpy as np
 import sys
-import tqdm
+from tqdm import tqdm
 trials = int(sys.argv[1])
 accounts = []
 try:
@@ -13,19 +13,22 @@ except FileNotFoundError:
 
     print("Getting Random Users")
     subjects = PushshiftIO.get_random_users(trials)
+    print(subjects)
     dataMatrix = []
     dataMatrix.append(["User", "Comment Karma", "Submission Karma", "Total Words", "Total Characters (not including spaces)"])
 
     print("Getting Data For Selected Users")
     for x, y, z in tqdm(subjects):
+        print(x)
         content = PushshiftIO.get_all_user_content(x)
         words_list = content.split(" ")
         character_count = 0
-        for x in words_list:
-            character_count += len(x)
+        for a in words_list:
+            character_count += len(a)
         user_statistics = [x, y, z, len(words_list), character_count]
         dataMatrix.append(user_statistics)
-
+    for x in dataMatrix:
+        print(x)
     final_data = pd.DataFrame(dataMatrix)
     final_data.to_csv("final_results.csv")
     print("Done!")
