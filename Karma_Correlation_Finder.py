e9ee16c4254f581e4f22a7415452bd8b760dfc15
Henry Tischler <mysterykeven@gmail.com>
2022-01-02 12:41:04 -0400

    Added progress bar to Karma_Correlation_Finder's pushshift.io queries

--- Karma_Correlation_Finder.py
+++ Karma_Correlation_Finder.py
@@ -4,24 +4,28 @@ from PushshiftIO import PushshiftIO
 import pandas as pd
 import numpy as np
 import sys
-trials = 2 #sys.argv[1]
+import tqdm
+trials = int(sys.argv[1])
 accounts = []
 try:
-    open("final_results.csv")
+    open("final_results.csv", "r")
 except FileNotFoundError:
 
+    print("Getting Random Users")
     subjects = PushshiftIO.get_random_users(trials)
     dataMatrix = []
-    dataMatrix.append(["user", "comment_karma", "submission_karma", "Total Words", "Total Characters (not including spaces)"])
+    dataMatrix.append(["User", "Comment Karma", "Submission Karma", "Total Words", "Total Characters (not including spaces)"])
 
-    for x in subjects:
+    print("Getting Data For Selected Users")
+    for x, y, z in tqdm(subjects):
         content = PushshiftIO.get_all_user_content(x)
         words_list = content.split(" ")
         character_count = 0
         for x in words_list:
             character_count += len(x)
-        user_statistics = [x[0], x[4], x[5], len(words_list), character_count]
+        user_statistics = [x, y, z, len(words_list), character_count]
         dataMatrix.append(user_statistics)
 
     final_data = pd.DataFrame(dataMatrix)
     final_data.to_csv("final_results.csv")
+    print("Done!")
