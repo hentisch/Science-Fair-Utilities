2feca64e1b2485e7f70ea4be5272aa85974eceda
Henry Tischler <mysterykeven@gmail.com>
2022-01-02 19:36:10 -0400

    Updated Karma_Correlation_Finder to write to csv in real time instead of holding in memory

--- Karma_Correlation_Finder.py
+++ Karma_Correlation_Finder.py
@@ -10,25 +10,16 @@ accounts = []
 try:
     open("final_results.csv", "r")
 except FileNotFoundError:
-
     print("Getting Random Users")
     subjects = PushshiftIO.get_random_users(trials)
-    print(subjects)
-    dataMatrix = []
-    dataMatrix.append(["User", "Comment Karma", "Submission Karma", "Total Words", "Total Characters (not including spaces)"])
-
-    print("Getting Data For Selected Users")
-    for x, y, z in tqdm(subjects):
-        print(x)
-        content = PushshiftIO.get_all_user_content(x)
-        words_list = content.split(" ")
-        character_count = 0
-        for a in words_list:
-            character_count += len(a)
-        user_statistics = [x, y, z, len(words_list), character_count]
-        dataMatrix.append(user_statistics)
-    for x in dataMatrix:
-        print(x)
-    final_data = pd.DataFrame(dataMatrix)
-    final_data.to_csv("final_results.csv")
-    print("Done!")
+    with open("final_results.csv", "w") as final_results:
+        final_results.writelines("User,Comment Karma,Submission Karma,Total Words,Total Characters (not including spaces)\n")
+        print("Getting Data For Selected Users")
+        for x, y, z in tqdm(subjects):
+            content = PushshiftIO.get_all_user_content(x)
+            words_list = content.split()
+            character_count = 0
+            for a in words_list:
+                character_count += len(a)
+            final_results.writelines(x + "," + str(y) + "," + str(z) + "," + str(len(words_list)) + "," + str(character_count) + "\n")
+        print("Done!")
