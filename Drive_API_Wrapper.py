e1a77e7fb602b29f00b581e4002aa4d00f6e9250
Henry Tischler <mysterykeven@gmail.com>
2022-01-05 00:34:46 -0500

    Fixed Profile_Downloader to not overwrite duplicates

--- Drive_API_Wrapper.py
+++ Drive_API_Wrapper.py
@@ -3,17 +3,23 @@ from pydrive.drive import GoogleDrive
 
 class Drive:
 
-    def __init__(self) -> None:
+    def __init__(self, folder_id:str = "1OMT-9fS3y3uuc-9WVGs6lYf-83fKbcFE") -> None:
         self.gauth = GoogleAuth()
         self.gauth.CommandLineAuth()
+        self.folder_id = folder_id
         #This whole proccess will force the user to login with Google O-Auth
         self.drive = GoogleDrive(self.gauth) 
     
     def add_file(self, title:str, content:str) -> None:
-        file1 = self.drive.CreateFile({'title': title, 'parents': [{'id': "1OMT-9fS3y3uuc-9WVGs6lYf-83fKbcFE"}]})  # Create GoogleDriveFile instance with title 'Hello.txt'.
+        file1 = self.drive.CreateFile({'title': title, 'parents': [{'id': self.folder_id}]})  # Create GoogleDriveFile instance with title 'Hello.txt'.
         #TODO figure out a more dynamic way to do file paths from PyDrive
         file1.SetContentString(content) # Set content of the file from given string.
         file1.Upload()
+    
+    def list_files(self) -> list:
+        file_list = self.drive.ListFile({'q': "'%s' in parents and trashed=false" % self.folder_id}).GetList()
+        return [x["originalFilename"] for x in file_list]
 
 if __name__ == "__main__":
     cool = Drive()
+    print(len(cool.list_files()))
--- Profile_Downloader.py
+++ Profile_Downloader.py
@@ -6,11 +6,16 @@ from Drive_API_Wrapper import Drive
 
 google_drive_storage = Drive()
 profile_count = int(sys.argv[1])
-accounts = []
+previous_accounts = set([x[:-4] for x in google_drive_storage.list_files()])
+duplicate_accounts = 0
+
 print("Getting Random Users")
 subjects = PushshiftIO.get_random_users(profile_count)
 print("Getting Data For Selected Users")
 for x, y, z in tqdm(subjects):
-    content = PushshiftIO.get_all_user_content(x)
-    google_drive_storage.add_file(f"{x}.txt", f"{x} {y} {z} \n {content}")
-print("Done!")
+    if not (x in previous_accounts):
+        content = PushshiftIO.get_all_user_content(x)
+        google_drive_storage.add_file(f"{x}.txt", f"{x} {y} {z} \n {content}")
+    else:
+        duplicate_accounts += 1
+print(f"Done! In total, {profile_count - duplicate_accounts} were added.")
