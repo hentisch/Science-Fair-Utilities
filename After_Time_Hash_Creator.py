ee3f3f851c0251a30104ff269690543032a97b22
Henry Tischler <mysterykeven@gmail.com>
2022-01-07 12:44:27 -0700

    Updated to stable build

--- After_Time_Hash_Creator.py
+++ After_Time_Hash_Creator.py
@@ -3,12 +3,13 @@ from Drive_API_Wrapper import Drive
 from hashlib import sha256
 
 def main():
-    gdrive = Drive()
-
-    with open("checksums.csv", "w") as checksum_file:
-        for n, id in gdrive.list_files():
-            checksum = sha256(gdrive.read_file(id)).hexdigest()
-            checksum_file.writeline(f"{n},{checksum}")
-        checksum_file.close()
-    
+    gdrive = Drive(folder_id="1tnhjKsh_wmRg1wsL0AABumsqzpfP76G-")
+    file_str = ""
+    for n, id in gdrive.list_files():
+        checksum = sha256(gdrive.read_file(id).encode('utf-8')).hexdigest()
+        file_str += (f"{n},{checksum} \n")
+    gdrive.add_file("checksums.txt", file_str)
     print("Done!")
+
+if __name__ == "__main__":
+    main()
