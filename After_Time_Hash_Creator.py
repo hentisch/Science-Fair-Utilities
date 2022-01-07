8d8b3fa74a9d293c60f2c1f76320d464f73b943f
Henry Tischler <mysterykeven@gmail.com>
2022-01-07 12:48:46 -0700

    Added progress bar

--- After_Time_Hash_Creator.py
+++ After_Time_Hash_Creator.py
@@ -5,7 +5,7 @@ from hashlib import sha256
 def main():
     gdrive = Drive()
     file_str = ""
-    for n, id in gdrive.list_files():
+    for n, id in tqdm(gdrive.list_files()):
         checksum = sha256(gdrive.read_file(id).encode('utf-8')).hexdigest()
         file_str += (f"{n},{checksum} \n")
     gdrive.add_file("checksums.txt", file_str)
