a5cf39283cf4751875fa6d766b416695e060c185
Henry Tischler <mysterykeven@gmail.com>
2022-01-06 19:50:02 -0600

    Added if name == main boiler plate

--- Karma_Correlation_Finder.py
+++ Karma_Correlation_Finder.py
@@ -5,14 +5,18 @@ from Drive_API_Wrapper import Drive
 
 #This file will take all of the txt files in the directory and create the spreadsheet from that
 
-gdrive = Drive()
+def main():
+        gdrive = Drive()
 
-with open("final_results.csv", "w") as final_results:
-    final_results.writelines("User,Comment Karma,Submission Karma,Total Words,Total Characters (not including spaces)\n")
-    for id in tqdm(gdrive.list_file_ids()):
-            content = gdrive.read_file(id)
-            body = content[content.index("\n") + 1:]
-            x, y, z = content[:content.index("\n")].split(" ")[0], content[:content.index("\n")].split(" ")[1], content[:content.index("\n")].split(" ")[2]
-            words_list = body.split()
-            final_results.writelines(x + "," + str(y) + "," + str(z) + "," + str(len(words_list)) + "," + str(len(body.replace("\n", ""))) + "\n")
-    print("Done!")
+        with open("final_results.csv", "w") as final_results:
+                final_results.writelines("User,Comment Karma,Submission Karma,Total Words,Total Characters (not including spaces)\n")
+                for id in tqdm(gdrive.list_file_ids()):
+                        content = gdrive.read_file(id)
+                        body = content[content.index("\n") + 1:]
+                        x, y, z = content[:content.index("\n")].split(" ")[0], content[:content.index("\n")].split(" ")[1], content[:content.index("\n")].split(" ")[2]
+                        words_list = body.split()
+                        final_results.writelines(x + "," + str(y) + "," + str(z) + "," + str(len(words_list)) + "," + str(len(body.replace("\n", ""))) + "\n")
+        print("Done!")
+
+if __name__ == "__main__":
+        main()
