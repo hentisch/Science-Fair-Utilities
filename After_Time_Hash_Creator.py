48ee7b9437ce4331c761ce2f273e6e66deac0878
Henry Tischler <mysterykeven@gmail.com>
2022-01-07 12:46:21 -0700

    Updated to work on main data directory

--- After_Time_Hash_Creator.py
+++ After_Time_Hash_Creator.py
@@ -3,7 +3,7 @@ from Drive_API_Wrapper import Drive
 from hashlib import sha256
 
 def main():
-    gdrive = Drive(folder_id="1tnhjKsh_wmRg1wsL0AABumsqzpfP76G-")
+    gdrive = Drive()
     file_str = ""
     for n, id in gdrive.list_files():
         checksum = sha256(gdrive.read_file(id).encode('utf-8')).hexdigest()
