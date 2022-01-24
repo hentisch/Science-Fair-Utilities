34ed3885121daa8bbf6facf1eead41348e6414e7
Henry Tischler <mysterykeven@gmail.com>
2022-01-23 23:19:36 -0700

    updated script to produce results sheet

--- Scripts/Python/Experimentation/imposters_testing.py
+++ Scripts/Python/Experimentation/imposters_testing.py
@@ -34,36 +34,38 @@ def sort_feature_matrices(arr:list):
 
 def main():
     #This block creates the feature matrices
+    complete_features = False
+
     try:
         files = os.listdir(f"feature-matrices({sys.argv[2]}-gram)") # This just checks if the directory exists, TODO implment the tests to run on each matrix
-    except FileNotFoundError or "-c" in sys.argv:
-        print("Feature matrices not found, creating...")
+        complete_features = len(files) == len(sys.argv[1])
+    except FileNotFoundError:   
+        pass
+    except IndexError:
+        print("Python3 imposterts_testing.py <path to directory> <ngram count> -c/none")
+        quit()
 
+    if not complete_features:
+        print("Feature matrices not found, creating...")
         try:
             os.mkdir(f"feature-matrices({sys.argv[2]}-gram)")
         except FileExistsError:
             pass
 
-        current_progress = len(os.listdir(f"feature-matrices({sys.argv[2]}-gram)"))
+        current_progress = os.listdir(f"feature-matrices({sys.argv[2]}-gram)")
+        source_directory = os.listdir(sys.argv[1])
+        absent_corpera = [x for x in source_directory if get_int_in_str(x) not in [get_int_in_str(y) for y in current_progress]]
 
-        with tqdm(total=len(os.listdir(sys.argv[1])), initial=current_progress) as pbar:
-            for i, x in enumerate(sort_feature_matrices(os.listdir(sys.argv[1]))):
-                raw_corpus = delta.Corpus(sys.argv[1] + "/" + x, ngrams=int(sys.argv[2])) #As this is the most computationally instensive step, we only want to do it once
-                trimmed_corpus = raw_corpus.cull(1/3)
-                with open(f"feature-matrices({sys.argv[2]}-gram)/features-{i+1}.pickle", "wb") as f:
-                    pkl.dump(trimmed_corpus, f)
-                pbar.update(1)
-    except IndexError:
-        print("Python3 imposterts_testing.py <path to directory> <ngram count> -c/none")
-        quit()
+        for i, x in enumerate(tqdm(sort_feature_matrices(os.listdir(sys.argv[1])))):
+            raw_corpus = delta.Corpus(sys.argv[1] + "/" + x, ngrams=int(sys.argv[2])) #As this is the most computationally instensive step, we only want to do it once
+            trimmed_corpus = raw_corpus.cull(1/3)
+            with open(f"feature-matrices({sys.argv[2]}-gram)/features-{i+1}.pickle", "wb") as f:
+                pkl.dump(trimmed_corpus, f)
     
-    try:
-        if sys.argv[3] == "-c":
-            quit()
-    except IndexError:
-        pass
+    if "-c" in sys.argv:
+        quit()
 
-    #This block 
+    #This block creats the distance matrices for each corpus
     try:
         os.listdir(f"distance-matrices/{sys.argv[2]}-gram")
     except FileNotFoundError:
@@ -71,22 +73,32 @@ def main():
         print("All dataframes loaded")
         
         print("Computing distances....")
-
         for x in ["distance-matrices", f"distance-matrices/{sys.argv[2]}-gram"]:
             try:
                 os.mkdir(x)
             except FileExistsError:
                 pass
         
-        distance_matrices = []
         for i, x in enumerate(tqdm(corpera)):
             with open(f"distance-matrices/{sys.argv[2]}-gram/distances-{i+1}.pickle", "wb") as f:
                 distances = delta.functions.cosine_delta(x)
                 pkl.dump(distances, f)
-                distance_matrices.append(distances)
         
         print("Distances computed!!!")
     
+    distances_matrices = []
+    for x in sort_feature_matrices(os.listdir(f"distance-matrices/{sys.argv[2]}-gram")):
+        distances_matrices.append(load_from_pickle(f"distance-matrices/{sys.argv[2]}-gram/{x}"))
+    with open("results.csv", "w") as f:
+        f.writelines("author,position,corpus")
+        for p, c in enumerate(distances_matrices):
+            for x in c.columns:
+                if x[:4] != "#TS#":
+                    df = distances[x]
+                    df = df.sort_values(ascending=True)
+                    df.drop(x, inplace=True) #In our correlation matrix, the author will ALWAYS be the first column, as the distance between x and x is 0
+                    authors = list(df.index.values)
+                    f.writelines(f"{x},{authors.index('#TS#' + x)},")
     """
     with open("results.csv", "w") as f:
         for x in distances.columns:
